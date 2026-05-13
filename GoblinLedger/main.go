2977dbf97569f059be1cabba7dafcfca14129331
XPenguinGodX <amadeo.2001@gmail.com>
2026-05-13 09:58:56 -0600

    Finished Up Get
    We are now able to get Goblins from database

--- GoblinLedger/main.go
+++ GoblinLedger/main.go
@@ -3,16 +3,25 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"gorm.io/driver/postgres"
-	"gorm.io/gorm"
 	"net/http"
 	"os"
+	"strconv"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 )
 
 type Goblin struct {
-	ID       uint `gorm:"primarykey"`
-	Username string
-	Email    string
+	ID       uint   `gorm:"primarykey"`
+	Username string `gorm:"unique;not null"`
+	Password string `gorm:"type:varchar(60);not null"`
+	Email    string `gorm:"unique;not null"`
+	Clan     string
+}
+
+type GrabbedGoblin struct {
+	ID       uint   `gorm:"primarykey"`
+	Username string `gorm:"unique;not null"`
 	Clan     string
 }
 
@@ -39,7 +48,8 @@ func main() {
 		fmt.Fprintf(w, "Goblins believe server is running")
 	})
 
-	multiplexer.HandleFunc("/NewGoblin", registerGoblin)
+	multiplexer.HandleFunc("/newgoblin/register", registerGoblin)
+	multiplexer.HandleFunc("/grabgoblin/{id}", getGoblin)
 
 	fmt.Println("Listening on port 8088...HOPEFULLY")
 	http.ListenAndServe(":8088", multiplexer)
@@ -63,4 +73,44 @@ func registerGoblin(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusCreated)
 	return
 
+}
+
+func deleteGoblin(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	if r.Method != http.MethodDelete {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+	}
+
+	// I WILL FINISH LATER. i need bearer token for this
+}
+
+func getGoblin(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	if r.Method != http.MethodGet {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		return
+	}
+
+	idStr := r.PathValue("id")
+
+	id, err := strconv.Atoi(idStr)
+	if err != nil {
+		http.Error(w, "Invalid Goblin ID format", http.StatusBadRequest)
+		return
+	}
+
+	goblin := GrabbedGoblin{}
+
+	outcome := db.Model(&Goblin{}).Select("id", "username", "clan").First(&goblin, id)
+
+	if outcome.Error != nil {
+		if outcome.Error == gorm.ErrRecordNotFound {
+			http.Error(w, "Goblin not found", http.StatusNotFound)
+			return
+		}
+		http.Error(w, "The Goblin Lords have forsaken you", http.StatusNotFound)
+		return
+	}
+
+	json.NewEncoder(w).Encode(goblin)
 }
