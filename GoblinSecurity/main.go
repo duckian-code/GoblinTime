8d7128c3d8e25a03d7b53d99bc3f1d0419a41da0
XPenguinGodX <amadeo.2001@gmail.com>
2026-05-15 23:45:28 -0600

    RoomJWT
    This is just the Token Generator in the documentation, its already perfect so why not use it

--- GoblinSecurity/main.go
+++ GoblinSecurity/main.go
@@ -1,1 +1,20 @@
 package main
+
+import (
+	"time"
+
+	"github.com/livekit/protocol/auth"
+)
+
+func getJoinToken(apiKey, apiSecret, room, identity string) (string, error) {
+	at := auth.NewAccessToken(apiKey, apiSecret)
+	grant := &auth.VideoGrant{
+		RoomJoin: true,
+		Room:     room,
+	}
+	at.SetVideoGrant(grant).
+		SetIdentity(identity).
+		SetValidFor(time.Hour)
+
+	return at.ToJWT()
+}
