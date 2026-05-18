74e6cd1187eeeecc010fda79d5542e4d151dcb75
Melanie Murray <melaniemurr13@gmail.com>
2026-05-17 22:41:59 -0600

    chore: put together auth skeleton

---
+++ GateKeeper/login.go
@@ -0,0 +1,110 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"goblinTime/data"
+	"log"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+)
+
+type LoginRequest struct {
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
+
+func main() {
+	if os.Getenv("JWT_SECRET") == "" {
+		log.Fatal("JWT_SECRET environment variable is required.")
+	}
+
+	if err := data.ConnectDatabase(); err != nil {
+		log.Fatalf("Auth service failed to connect to DB: %v", err)
+	}
+
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/users/login", LoginHandler)
+	mux.HandleFunc("/validate", validateHandler)
+
+	fmt.Println("Auth service listening on port 8088")
+	log.Fatal(http.ListenAndServe(":8088", mux))
+}
+
+func LoginHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Error(w, "Method not allowed", http.StatusBadRequest)
+		return
+	}
+
+	var lr LoginRequest
+	if err := json.NewDecoder(r.Body).Decode(&lr); err != nil {
+		http.Error(w, "Invalid request", http.StatusBadRequest)
+		return
+	}
+
+	userID := data.VerifyUser(lr.Email, lr.Password)
+	if userID == -1 {
+		http.Error(w, "Invalid Email or Password", http.StatusUnauthorized)
+		return
+	}
+
+	token, err := generateToken(userID)
+	if err != nil {
+		http.Error(w, "Error Generating Token", http.StatusInternalServerError)
+		return
+	}
+	json.NewEncoder(w).Encode(map[string]string{"token": token})
+}
+
+func validateHandler(w http.ResponseWriter, r *http.Request) {
+	authHeader := r.Header.Get("Authorization")
+	if authHeader == "" {
+		w.WriteHeader(http.StatusUnauthorized)
+		return
+	}
+
+	tokenString := ""
+	if len(authHeader) > 7 && authHeader[:7] == "Bearer " {
+		tokenString = authHeader[7:]
+	}
+
+	claims := jwt.MapClaims{}
+
+	token, err := jwt.ParseWithClaims(tokenString, claims, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("unexpected signing method")
+		}
+		return []byte(os.Getenv("JWT_SECRET")), nil
+	})
+
+	if err != nil || !token.Valid {
+		w.WriteHeader(http.StatusUnauthorized)
+		return
+	}
+
+	if userID, ok := claims["sub"].(float64); ok {
+		w.Header().Set("X-User-ID", fmt.Sprintf("%.0f", userID))
+		w.WriteHeader(http.StatusOK)
+		return
+	}
+
+	w.WriteHeader(http.StatusUnauthorized)
+}
+
+func generateToken(userID int) (string, error) {
+
+	claims := jwt.MapClaims{
+		"sub": userID,
+		"exp": time.Now().Add(time.Hour * 24).Unix(),
+		"iat": time.Now().Unix(),
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+
+	return token.SignedString([]byte(os.Getenv("JWT_SECRET")))
+}
